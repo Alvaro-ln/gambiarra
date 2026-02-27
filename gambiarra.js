30d103b770702443e8986cdb89fdc150fe89cef3
Alvaro-ln <alvaro.otrabalhador@gmail.com>
2026-02-27 16:12:12 +0000

    correção v2

--- gambiarra.js
+++ gambiarra.js
@@ -1,1 +1,33 @@
-javascript:(function(){var h=document.querySelector('#interaction-header-participant-name');if(!h)return;var t=h.innerHTML;var r=/\[(\d+)\]/g;h.innerHTML=t.replace(r,function(m,id){var u='https://novorevan.brisanet.net.br/#/venda/cliente/'+id+'/sobre';return'[<a href="'+u+'" target="_blank" style="color:#00bfff;font-weight:bold;">'+id+'</a>]';});})();
+javascript:(function(){
+if(window.__clienteObserverAtivo)return;
+window.__clienteObserverAtivo=true;
+
+function aplicarLink(){
+  const h=document.querySelector('#interaction-header-participant-name');
+  if(!h)return;
+
+  const textoOriginal=h.textContent;
+
+  const regex=/\[(\d+)\]/g;
+
+  const novoHTML=textoOriginal.replace(regex,function(match,id){
+    const url='https://novorevan.brisanet.net.br/#/venda/cliente/'+id+'/sobre';
+    return '[<a href="'+url+'" target="_blank" style="color:#00bfff;font-weight:bold;">'+id+'</a>]';
+  });
+
+  if(h.innerHTML!==novoHTML){
+    h.innerHTML=novoHTML;
+  }
+}
+
+aplicarLink();
+
+const observer=new MutationObserver(function(){
+  aplicarLink();
+});
+
+observer.observe(document.body,{
+  childList:true,
+  subtree:true
+});
+})();
